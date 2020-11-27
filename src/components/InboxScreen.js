1e966be3595d1d338d6f435a4ecf5ee7ce524af4
hyeonju Lee <hyeonju@thingsw.com>
2020-11-27 16:54:08 +0900

    taskbox UI2

--- src/components/InboxScreen.js
+++ src/components/InboxScreen.js
@@ -1,17 +1,43 @@
+// src/components/InboxScreen.js
+
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
-import { PureInboxScreen } from './InboxScreen';
+import TaskList from './TaskList';
 
-export default {
-    component: PureInboxScreen,
-    title: 'InboxScreen',
+export function PureInboxScreen({ error }) {
+    if (error) {
+        return (
+            <div className="page lists-show">
+                <div className="wrapper-message">
+                    <span className="icon-face-sad" />
+                    <div className="title-message">Oh no!</div>
+                    <div className="subtitle-message">Something went wrong</div>
+                </div>
+            </div>
+        );
+    }
+
+    return (
+        <div className="page lists-show">
+            <nav>
+                <h1 className="title-page">
+                    <span className="title-wrapper">Taskbox</span>
+                </h1>
+            </nav>
+            <TaskList />
+        </div>
+    );
+}
+
+PureInboxScreen.propTypes = {
+    /** The error message */
+    error: PropTypes.string,
 };
 
-const Template = args => <PureInboxScreen {...args} />;
-
-export const Default = Template.bind({});
-
-export const Error = Template.bind({});
-Error.args = {
-    error: 'Something',
+PureInboxScreen.defaultProps = {
+    error: null,
 };
+
+export default connect(({ error }) => ({ error }))(PureInboxScreen);
